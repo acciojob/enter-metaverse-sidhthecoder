89367f70a25c400ffd65bf57c676f6c2b4239539
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-05-20 16:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const statusText = document.getElementById('status');
+  const enterBtn = document.getElementById('enterBtn');
+
+  enterBtn.addEventListener('click', function() {
+    statusText.textContent = 'Entered Metaverse';
+
+    const h1 = document.createElement('h1');
+    h1.textContent = statusText.textContent;
+
+    statusText.parentNode.replaceChild(h1, statusText);
+  });
+});
